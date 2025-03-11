b389807688c51f3eb8a16fd1463ccdd9026bd7bc
wonhyeong <wonhyeong@wonhyeongui-MacBookPro.local>
2025-03-11 22:15:09 +0900

    리팩토링(2) - io

---
+++ src/main/java/cleancode/studycafe/tobe/io/StudyCafeIOHandler.java
@@ -0,0 +1,46 @@
+package cleancode.studycafe.tobe.io;
+
+import cleancode.studycafe.tobe.model.StudyCafeLockerPass;
+import cleancode.studycafe.tobe.model.StudyCafePass;
+import cleancode.studycafe.tobe.model.StudyCafePassType;
+
+import java.util.List;
+
+public class StudyCafeIOHandler {
+    private final InputHandler inputHandler = new InputHandler();
+    private final OutputHandler outputHandler = new OutputHandler();
+
+    public void showWelcomeMessage() {
+    }
+
+    public void showAnnouncement() {
+    }
+
+    public void showPassOrderSummary(StudyCafePass selectedPass, StudyCafeLockerPass lockerPass) {
+
+    }
+
+    public void showPassOrderSummary(StudyCafePass selectedPass) {
+
+    }
+
+    public void showSimpleMessage(String message) {
+
+    }
+
+    public StudyCafePassType askPassTypeSelecting() {
+        outputHandler.askPassTypeSelection();
+        return inputHandler.getPassTypeSelectingUserAction();
+    }
+
+    public StudyCafePass askPassSelecting(List<StudyCafePass> passCandidates) {
+        outputHandler.showPassListForSelection(passCandidates);
+        return inputHandler.getSelectPass(passCandidates);
+    }
+
+    public boolean askLockerPass(StudyCafeLockerPass lockerPassCandidate) {
+        outputHandler.askLockerPass(lockerPassCandidate);
+        return inputHandler.getLockerSelection();
+
+    }
+}
