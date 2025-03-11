7a5d897a36127e1dca59bdb3bc373aa285651886
김원형 KIM <wonhyeong.kim@jcetglobal.com>
2025-03-11 11:35:16 +0900

    Add Mine2 Package retry Refactoring

---
+++ src/main/java/cleancode/studycafe/mine2/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.mine2;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
